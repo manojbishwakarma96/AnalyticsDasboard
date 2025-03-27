3987ef64c21fde353632f3ed3fc89a6174ce27c3
manojbishwakarma <manojbishwakarma@neebal.com>
2025-03-27 17:58:02 -0400

    Add environment variables support with dotenv

--- AnalyticsApp/database/database.js
+++ AnalyticsApp/database/database.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://manojbishwakarma88:manoj123@analytics.jf80u9m.mongodb.net/analyticsDB"
-    );
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected...");
   } catch (err) {
     console.error("Database connection failed", err);
--- AnalyticsApp/index.js
+++ AnalyticsApp/index.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const analyticsRoutes = require('./routes/analyticsRoutes');
-const connectDB = require('./database/database');
+const express = require("express");
+const mongoose = require("mongoose");
+const analyticsRoutes = require("./routes/analyticsRoutes");
+const connectDB = require("./database/database");
+require("dotenv").config();
 const app = express();
 
 connectDB();
 //Use analytics route
-app.use('/',analyticsRoutes);
+app.use("/", analyticsRoutes);
 
 //start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT,()=>{
-    console.log(`Server running on http://localhost:${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
